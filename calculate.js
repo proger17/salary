a31c301ab66ff623d601d8b4241effaeef354746
proger_17 <136320150+proger17@users.noreply.github.com>
2025-05-12 21:22:10 +0300

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -108,106 +108,120 @@ function calculateRow(row) {
     const issued_by_employees = parseFloat(row.querySelector('input[name="issued_by_employees[]"]').value) || 0;
     const out_of_staff = parseFloat(row.querySelector('input[name="out_of_staff[]"]').value) || 0;
 
-    const fixed_salary = Math.min(number_of_shifts, 15) * 3000;
+    const fixed_salary = number_of_shifts * 3000;
     const CR2 = (parseFloat(out_of_staff) / parseFloat(issued_by_employees)) * 100;
 
-    var first_bonus_1 = 0;
-    var second_bonus_1 = 30;
+    var i1 = 0;
+    var j1 = 0;
+
+    var b11 = [
+        0, 100, 150, 180
+    ];
+
+    var b12 = [
+        [20, 140, 160, 180, 200],
+        [25, 150, 170, 190, 210],
+        [30, 160, 180, 200, 220],
+        [35, 170, 190, 210, 230]
+    ];
     
     switch (true) {
         case (issued_by_employees <= 99):
-            first_bonus_1 = 0;
-            second_bonus_1 = second_bonus_1 + 0;
+            i1 = 0
             break;
         case (100 <= issued_by_employees && issued_by_employees <= 149):
-            first_bonus_1 = 100;
-            second_bonus_1 = second_bonus_1 + 10;
+            i1 = 1;
             break;
         case (150 <= issued_by_employees && issued_by_employees <= 199):
-            first_bonus_1 = 150;
-            second_bonus_1 = second_bonus_1 + 20;
+            i1 = 2;
             break;
         case (issued_by_employees >= 200):
-            first_bonus_1 = 200;
-            second_bonus_1 = second_bonus_1 + 30;
+            i1 = 3;
             break;
     }
 
     switch (true) {
         case (CR2 < 50 || CR2 >= 85):
-            second_bonus_1 = 0;
+            j1 = -1;
             break;
         case (50 <= CR2 && CR2 < 55):
-            second_bonus_1 = second_bonus_1 + 0;
+            j1 = 0;
             break;
         case (55 <= CR2 && CR2 < 60):
-            second_bonus_1 = second_bonus_1 + 20;
+            j1 = 1;
             break;
         case (60 <= CR2 && CR2 < 65):
-            second_bonus_1 = second_bonus_1 + 40;
+            j1 = 2;
             break;
         case (65 <= CR2 && CR2 < 70):
-            second_bonus_1 = second_bonus_1 + 60;
+            j1 = 3;
             break;
-        case (70 <= CR2 && CR2 < 75):
-            second_bonus_1 = second_bonus_1 + 80;
-            break;
-        case (75 <= CR2 && CR2 < 80):
-            second_bonus_1 = second_bonus_1 + 100;
+        case (70 <= CR2):
+            j1 = 4;
             break;
     }
 
-    var first_bonus_2 = 0;
-    var second_bonus_2 = 200;
+
+    var i2 = 0;
+    var j2 = 0;
+
+    var b21 = [
+        0, 30, 50
+    ];
+
+    var b22 = [
+        [70, 220, 270, 320, 370],
+        [80, 230, 280, 330, 380],
+        [90, 240, 290, 340, 390]
+    ];
 
     switch (true) {
         case (out_of_staff <= 69):
-            first_bonus_2 = 0;
-            second_bonus_2 = second_bonus_2 + 0;
+            i2 = 0;
             break;
         case (70 <= out_of_staff && out_of_staff <= 99):
-            first_bonus_2 = 50;
-            second_bonus_2 = second_bonus_2 + 100;
+            i2 = 1;
             break;
         case (out_of_staff >= 100):
-            first_bonus_2 = 100;
-            second_bonus_2 = second_bonus_2 + 200;
+            i2 = 2;
             break;
     }
 
     switch (true) {
         case (0 <= CR2 && CR2 < 50):
-            first_bonus_2 = first_bonus_2;
-            second_bonus_2 = 0;
+            j2 = -1;
             break;
         case (50 <= CR2 && CR2 < 55):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 0;
+            j2 = 0;
             break;
         case (55 <= CR2 && CR2 < 60):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 50;
+            j2 = 1;
             break;
         case (60 <= CR2 && CR2 < 65):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 100;
+            j2 = 2;
             break;
         case (65 <= CR2 && CR2 < 70):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 150;
+            j2 = 3;
             break;
-        case (70 <= CR2 && CR2 < 75):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 200;
-            break;
-        case (75 <= CR2 && CR2 < 80):
-            first_bonus_2 = first_bonus_2 * 0;
-            second_bonus_2 = second_bonus_2 + 250;
+        case (70 <= CR2):
+            j2 = 4;
             break;
     }
 
-    const bonus_1 = (first_bonus_1 + second_bonus_1) * issued_by_employees;
-    const bonus_2 = (first_bonus_2 + second_bonus_2) * out_of_staff;
+    var bonus_1 = 0;
+    var bonus_2 = 0;
+
+    if (j1 == -1) {
+        bonus_1 = b11[i1] * issued_by_employees;
+    } else {
+        bonus_1 = (b11[i1] + b12[i1][j1]) * issued_by_employees;
+    }
+
+    if (j2 == -1) {
+        bonus_2 = b21[i2] * out_of_staff;
+    } else {
+        bonus_2 = b22[i2][j2] * out_of_staff;
+    }
 
     const salary = fixed_salary + bonus_1 + bonus_2;
 
